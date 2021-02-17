079166c9ec88392aedeb3b2f1264cbe21d4d4752
IRoye <yuyi@newdt.cn>
2021-02-17 17:01:12 +0800

    不将原值转为字符串的回文

--- src/leetcode/IsPalindrome.java
+++ src/leetcode/IsPalindrome.java
@@ -23,7 +23,33 @@ public class IsPalindrome {
 
     ;
 
+    /**
+     * @param x
+     * @return
+     * @description: 不转为字符串进行计算
+     */
+    public static boolean isPalindrome2(int x) {
+        if (x < 0 || (x % 10 == 0 && x > 10) ) return false;
+//       步骤：
+//        将原数x % 10 取得倒数第一位，假如是12321，那么 12321 % 10 = 1，获取到最后一位，接下来，x = x / 10 = 1232。继续步骤，x % 10 = 2，此时拿到倒数第二位，
+//        此时x = x / 10 = 123。如何判断
+        int x1 = 0;
+//        如果是12321 这样的数的话，那么那么123 = （x1 = x1 * 10 + x % 10）/ 10 如果是1221 那么直接就是 12 = （x1 = x1 * 10 + x % 10）
+        while (x > x1) {
+            x1 = x1 * 10 + x % 10;
+            x = x / 10;
+        }
+        System.out.println(x);
+        System.out.println(x1);
+        if (x == x1 / 10 || x == x1) return true;
+
+        return false;
+
+    }
+
+    ;
+
     public static void main(String[] args) {
-        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome2(1));
     }
 }
